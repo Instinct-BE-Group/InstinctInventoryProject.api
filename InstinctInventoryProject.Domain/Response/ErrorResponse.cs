f1e4b633d13072ac8b38bcfffcb6b245d561060a
YUSSUFEO <olawale.yussuf@sterling.ng>
2023-10-11 09:37:08 +0100

    updated

---
+++ InstinctInventoryProject.Domain/Response/ErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace InstinctInventoryProject.Domain.Response
+{
+    public class ErrorResponse
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ InstinctInventoryProject.Domain/Response/ResponseDate.cs
@@ -0,0 +1,8 @@
+﻿namespace InstinctInventoryProject.Domain.Response
+{
+    public class ResponseDate
+    {
+        public string ResponseCode { get; set; }
+        public string Response { get; set; }
+    }
+}
